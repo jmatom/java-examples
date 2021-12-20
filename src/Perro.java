a97bce0856f1796e02f1f6c7d8a465aa6f4bad29
Jose Mato <matinho@gmail.com>
2021-12-20 12:24:59 +0100

    add perro

--- src/Perro.java
+++ src/Perro.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Perro {
+// Declaración de clase
+
+public class Perro
+{
+    // Variables de instancia
+    String nombre;
+    String raza;
+    int edad;
+    String color;
+
+    // Declaración del constructor de clase
+    public Perro(String nombre, String raza,
+                 int edad, String color)
+    {
+        this.nombre = nombre;
+        this.raza= raza;
+        this.edad = edad;
+        this.color = color;
+    }
+
+    // método 1
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    // método 2
+    public String getRaza()
+    {
+        return raza;
+    }
+
+    // método 3
+    public int getEdad()
+    {
+        return edad;
+    }
+
+    // método 4
+    public String getColor()
+    {
+        return color;
+    }
+
+    @Override
+    public String toString()
+    {
+        return("Hola mi nombre es "+ this.getNombre()+
+                ".\nMi raza, edad y color son: " +
+                this.getRaza()+"," + this.getEdad()+
+                ","+ this.getColor());
+    }
 }
